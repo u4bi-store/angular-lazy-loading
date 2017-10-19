b46cbc3797cea6d43316d81d15d5df62b724b40a
u4bi <rootcode10@gmail.com>
2017-10-20 06:31:53 +0900

    two routing module render

--- src/app/two/two-routing.module.ts
+++ src/app/two/two-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TwoComponent } from './two.component';
+import { TwoChildComponent } from './two-child/two-child.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TwoComponent,
+    children: [
+      {
+        path: 'two-child',
+        component: TwoChildComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  declarations: [TwoComponent, TwoChildComponent],
+  exports: [RouterModule, TwoComponent, TwoChildComponent]
 })
 export class TwoRoutingModule { }
--- src/app/two/two.module.ts
+++ src/app/two/two.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TwoRoutingModule } from './two-routing.module';
-import { TwoComponent } from './two.component';
-import { TwoChildComponent } from './two-child/two-child.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TwoRoutingModule
-  ],
-  declarations: [TwoComponent, TwoChildComponent]
+  ]
 })
 export class TwoModule { }
