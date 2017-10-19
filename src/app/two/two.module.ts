5341090b57ff48ab7617f5bd50368ee30ebb93c8
u4bi <rootcode10@gmail.com>
2017-10-20 06:21:23 +0900

    ng g module --routing true two

---
+++ src/app/two/two.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TwoRoutingModule } from './two-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TwoRoutingModule
+  ],
+  declarations: []
+})
+export class TwoModule { }
